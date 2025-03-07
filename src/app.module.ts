18b10f1e147ca90a1db9f9a8d638d6d5bf8adc8c
Ashvin Kumbhani <akumbhani66@gmail.com>
2025-03-08 02:41:17 +0530

    removed global caching

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
 
 @Module({
-  imports: [
-    CacheModule.register({
-      isGlobal: true,
-      ttl: 300000, // 5 minutes in milliseconds
-    }),
-    AuthorModule
-  ],
+  imports: [AuthorModule],
   controllers: [AppController],
   providers: [AppService],
 })
